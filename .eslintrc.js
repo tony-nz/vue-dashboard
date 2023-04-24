a5738fc02ae8abf53baf755ec152ed7e0a732eff
Tony Myers <tony@myers.co.nz>
2023-04-24 17:28:17 +1200

    Adding in template, adjusting style, adding headless

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,10 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    'prettier'
-  ],
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
-    'vue/require-default-prop': 'off',
-  }
-}
+    "vue/require-default-prop": "off",
+  },
+};
